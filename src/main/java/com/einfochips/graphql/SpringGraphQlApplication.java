3ef9ba1574c75100fe2836a20fe0d604d4428ade
Dwarkesh Dhamecha <dwarkeshdhamecha2582@gmail.com>
2023-11-22 18:08:22 +0530

    Added Caching in Service

--- src/main/java/com/einfochips/graphql/SpringGraphQlApplication.java
+++ src/main/java/com/einfochips/graphql/SpringGraphQlApplication.java
@@ -3,11 +3,11 @@ package com.einfochips.graphql;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin("*")
+@EnableCaching
 public class SpringGraphQlApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/einfochips/graphql/service/impl/BookServiceImpl.java
+++ src/main/java/com/einfochips/graphql/service/impl/BookServiceImpl.java
@@ -2,7 +2,12 @@ package com.einfochips.graphql.service.impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.einfochips.graphql.entity.Book;
@@ -12,6 +17,8 @@ import com.einfochips.graphql.service.BookService;
 @Service
 public class BookServiceImpl implements BookService {
 
+	Logger logger = LoggerFactory.getLogger(BookService.class);
+	
 	@Autowired
 	BookRepo repo;
 
@@ -26,14 +33,18 @@ public class BookServiceImpl implements BookService {
 	}
 
 	@Override
+	@Cacheable(cacheNames = "books", key = "#id")
 	public Book getBook(String id) {
+		logger.info("Fetch Book: "+id);
 		return repo.findById(id).orElseThrow(() -> new RuntimeException("book not found with id: " + id));
 	}
 
 	@Override
+	@CacheEvict(cacheNames = "books", key = "#id")
 	public Boolean deleteBook(String id) {
 		if (repo.existsById(id)) {
 			repo.deleteById(id);
+			logger.warn("Book deleted: "+id);
 			return true;
 		}
 		return false;
@@ -41,11 +52,12 @@ public class BookServiceImpl implements BookService {
 	}
 
 	@Override
+	@CachePut(cacheNames = "books", key = "#book.id")
 	public Book update(Book book) {
 		if(!repo.existsById(book.getId())) {
 			throw new RuntimeException("book not found with id: "+book.getId());
 		}
-		
+		logger.info("Book Updated: "+book.getId());
 		return repo.save(book);
 	}
 
